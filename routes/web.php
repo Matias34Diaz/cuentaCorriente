93da919ae93b51512a017bda817b63b833abbc88
Matias34Diaz <matiasfedericodiaz98@gmail.com>
2024-02-12 07:04:16 -0300

    elimine auth que agregue

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\CursoController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use Psy\CodeCleaner\ReturnTypePass;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\CondicionFiscalController;
+use App\Http\Controllers\CuentaController;
+use App\Http\Controllers\PagoController;
 
 
 /*
@@ -16,5 +17,31 @@ use Psy\CodeCleaner\ReturnTypePass;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/', function () {
+    // Si el usuario ya está autenticado, redirigirlo a la página de inicio
+    if (Auth::check()) {
+        return redirect('/home'); // O a cualquier otra página de inicio
+    }
+    // De lo contrario, mostrar la vista de inicio de sesión
+    return view('auth.login');
+})->name('login');
+
+Auth::routes();
+
+
+Route::resource('cliente', App\Http\Controllers\ClienteController::class)->middleware('auth');
+Route::resource('condicion-fiscal', App\Http\Controllers\CondicionFiscalController::class)->middleware('auth');
+Route::resource('cuentas', App\Http\Controllers\CuentaController::class)->middleware('auth');
+Route::resource('servicios', App\Http\Controllers\ServicioController::class)->middleware('auth');
+Route::resource('pagos', App\Http\Controllers\PagoController::class)->middleware('auth');
+
+// ->middleware('auth');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+
+
 
 //
